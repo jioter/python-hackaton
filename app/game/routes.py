d0e657af718d8debcbfc13745762d1f20fff7c4f
Andrey Pesotskiy <andrepesotskiy@gmail.com>
2019-07-06 13:19:45 +0300

    feat: game module

---
+++ app/game/routes.py
@@ -0,0 +1,18 @@
+from flask_restful import Resource
+from auth.parsers import auth_parser
+
+user = dict()
+
+
+class Game(Resource):
+    def post(self):
+        pass
+
+    def get(self, game_id):
+        pass
+
+    def delete(self, game_id):
+        pass
+
+    def put(self, game_id):
+        pass
