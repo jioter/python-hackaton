d75e0aa9b20fd72df701fb526634c2ba308dcc92
Andrey Pesotskiy <andrepesotskiy@gmail.com>
2019-07-06 13:07:02 +0300

    fix: app

---
+++ app/auth/forms.py
@@ -0,0 +1,73 @@
+from flask_wtf import FlaskForm
+# from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
+
+
+# id = db.Column(db.Integer, primary_key=True)
+# login = db.Column(db.String, unique=True)
+# password = db.Column(db.String, unique=False)
+# role = db.Column(db.Integer, default=0)
+# create_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+
+
+
+class RegistrationForm(FlaskForm):
+    login = StringField("Login", validators=[DataRequired(), Length(min=2, max=2000)])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_login(self, login):
+        user = User.query.filter_by(login=login.data).first()
+        if user:
+            raise ValidationError("This username is taken.")
+    #
+    # def validate_email(self, email):
+    #     user = User.query.filter_by(email=email.data).first()
+    #     if user:
+    #         raise ValidationError("This email is taken.")
+
+
+class LoginForm(FlaskForm):
+    login = StringField("Login", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('LogIn')
+
+
+#
+# class UpdateAccountForm(FlaskForm):
+#     username = StringField("Username", validators=[DataRequired(), Length(min=2, max = 20)])
+#     email = StringField("Email", validators=[DataRequired(), Email()])
+#     # picture = FileField("Update Profile Picture", validators=[FileAllowed(['jpg', 'png'])])
+#     submit = SubmitField('Update')
+#
+#     def validate_username(self, username):
+#         if username.data != current_user.username:
+#             user = User.query.filter_by(username=username.data).first()
+#             if user:
+#                 raise ValidationError("This username is taken.")
+#
+#     def validate_email(self, email):
+#         if email.data != current_user.email:
+#             user = User.query.filter_by(email=email.data).first()
+#             if user:
+#                 raise ValidationError("This email is taken.")
+
+# class RequestResetForm(FlaskForm):
+#     email = StringField("Email", validators=[DataRequired(), Email()])
+#     submit = SubmitField('Request Password Reset')
+#
+#     def validate_email(self, email):
+#         user = User.query.filter_by(email=email.data).first()
+#         if user is None:
+#             raise ValidationError("There is no account with those email.")
+#
+# class ResetPasswordForm(FlaskForm):
+#     password = PasswordField("Password", validators=[DataRequired()])
+#     confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password')])
+#     submit = SubmitField('Reset Password')
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,20 +1,110 @@
-from flask_restful import Resource
-from flask import session
-from auth.parsers import auth_parser
+from flask import render_template, url_for, flash, redirect, request, Blueprint
+from flaskblog import, bcrypt
+from flaskblog.users.forms import (LoginForm, RegistrationForm, UpdateAccountForm,
+                             RequestResetForm, ResetPasswordForm)
+from flaskblog.models import User
+from flask_login import login_user, current_user, logout_user, login_required
+from flaskblog.users.utils import save_picture, save_post_picture, send_reset_email
 
-user = dict()
-
-
-class Login(Resource):
-    def post(self):
-        pass
-
-
-class Logout(Resource):
-    def get(self):
-        pass
+users = Blueprint('users', __name__)
 
 
-class Registration(Resource):
-    def post(self):
-        pass
+@users.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Your acc created! You can login", 'success')
+        return redirect(url_for('users.login'))
+    return render_template('00_register.html', title="Login-admin", form=form)
+
+
+@users.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        # flash(f"Welcome, {form.username.data}!", 'success')
+        # return redirect(url_for('main.home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        # password = User.query.filter_by(password=form.password.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('main.home'))
+        else:
+            flash("Login Unsuccessful. Please, go home and die!", 'danger')
+    return render_template('00_login.html', title="Login-admin", form=form)
+    # return render_template('00_admin.html', posts = posts)
+
+
+@users.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
+
+
+@users.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your acc has been updated!', 'success')
+        return redirect(url_for('users.account'))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('00_account.html', title="Account", image_file=image_file, form=form)
+
+
+@users.route("/user/<string:username>")
+def user_post(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page', 1, type=int)
+    posts = News.query.filter_by(author=user).order_by(News.pub_date.desc()).paginate(page=page, per_page=4)
+    return render_template('user_posts.html', posts=posts, user=user)
+
+
+@users.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been sent with instraction', 'info')
+        return redirect(url_for('users.login'))
+    return render_template('reset_request.html', title='Reset Password', form=form)
+
+
+@users.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash("That is an invalid token", 'warning')
+        return redirect(url_for('users.reset_request'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash("Your pass has been updated! You can login", 'success')
+        return redirect(url_for('users.login'))
+    return render_template('00_register.html', title="Login-admin", form=form)
