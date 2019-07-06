f9b6b4d2820240d7824681cebbe5090e13facd63
Pavlo Pavlenko <alskarf@gmail.com>
2019-07-06 13:55:36 +0300

    new requirements

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,23 @@
 from datetime import timedelta
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 from app.auth import auth
 from app.game import game
 from app.app import app
 
+bcrypt = Bcrypt()  # for password hashing
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
 
 def run_app():
+
     db.init_app(app)
     app.permanent_session_lifetime = timedelta(minutes=20)  # add session expire time
+
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
 
     app.register_blueprint(auth)
     app.register_blueprint(game)
