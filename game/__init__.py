62e5f8374fc65f4087acf7253513c30d1c9d5107
Andrey Pesotskiy <andrepesotskiy@gmail.com>
2019-07-06 16:29:52 +0300

    feat: add game

--- game/__init__.py
+++ game/__init__.py
@@ -1,29 +1,52 @@
-from flask import Blueprint
-from flask_restful import Api
+from flask import Blueprint, render_template, request, redirect
+from models import Game
+from game.game_form import GameForm
+from db import db
 
-game = Blueprint("game", __name__)
+game = Blueprint("game", __name__, template_folder='templates')
 
 
 @game.route("/game", methods=["GET", "POST", "DELETE"])
 def game_page():
-    # if request.method == "POST" and request.form['_method'] == "POST":
-    #     if request.form['id']:
-    #         update_fruit()
-    #     else:
-    #         create_fruit()
-    # elif request.method == "POST" and request.form['_method'] == "DELETE":
-    #     remove_fruit()
+    form = GameForm()
+    errors = None
+    if form.is_submitted() and not form.validate():
+        errors = form.errors
 
-    return 'Game'
+    if request.method == "POST" and request.form['_method'] == "POST":
+        result = create_game()
+        if result:
+            return redirect('/')
+
+    elif request.method == "POST" and request.form['_method'] == "DELETE":
+        remove_game()
+
+    return render_template('form.html', form=form, errors=errors)
 
 
-def update_fruit():
+def update_game():
     pass
 
 
-def create_fruit():
-    pass
+def create_game():
+    from_number = request.form.get('from_number') or 0
+    to_number = request.form.get('to_number') or 10
+    attempts = request.form.get('attempts') or 3
+
+    new_game = Game(
+        number=request.form['number'],
+        user_id=request.form['user_id'],
+        from_number=from_number,
+        to_number=to_number,
+        attempts=attempts,
+        status=Game.STATUS_ACTIVE
+    )
+
+    db.session.add(new_game)
+    db.session.commit()
+
+    return new_game.id
 
 
-def remove_fruit():
+def remove_game():
     pass
---
+++ game/game_form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField
+from wtforms.validators import DataRequired
+
+
+class GameForm(FlaskForm):
+    number = IntegerField('number', validators=[DataRequired()])
+    from_number = IntegerField('from_number')
+    to_number = IntegerField('to_number')
+    attempts = IntegerField('attempts')
